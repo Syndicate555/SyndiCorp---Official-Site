44a33b4bfb9fdb918b5f65f6de0680f44383d547
Syndicate555 <saziz028@uottawa.ca>
2020-07-04 05:21:13 -0400

    changes

--- syndicorp/src/App.js
+++ syndicorp/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Header from "./components/Header/Header";
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
--- syndicorp/src/components/Header/Header.js
+++ syndicorp/src/components/Header/Header.js
@@ -22,7 +22,7 @@ export default class Navbar extends Component {
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
-              Syndicate Store
+              SyndiCorp
             </Link>
           </li>
         </ul>
